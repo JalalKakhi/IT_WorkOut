e69eb607c0765667321250c30d7e490ab2c8561c
jalal <galalkakhi@gmail.com>
2025-08-07 19:36:22 +0200

    Add setting , bmi , and Notification screens

---
+++ lib/IT_workout/shared/notif_system/notification.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotiSevice {
+  final notifactionPlugin = FlutterLocalNotificationsPlugin();
+
+  bool _isInit = false;
+
+  bool get isInit => _isInit;
+  // Init
+  Future<void> initNotifaction() async {
+    if (_isInit) return;
+
+    //prepare the android init
+    const initSettingAndroid =
+        AndroidInitializationSettings('@mipmap/ic_lancher');
+
+    // init setting
+    const initSetting = InitializationSettings(android: initSettingAndroid);
+
+// finally init the plugin
+    await notifactionPlugin.initialize(initSetting);
+  }
+
+  //Notifaction details
+  NotificationDetails notificationDetails() {
+    return const NotificationDetails(
+        android: AndroidNotificationDetails(
+            'daily_channel_id', 'Daily Notification',
+            channelDescription: 'Daily Notification Channel',
+            importance: Importance.max,
+            priority: Priority.high));
+  }
+}
